f5e1a474f863c35eae75e5c29d9898d64e271191
DeployChef <dc.titovvlad@gmail.com>
2020-09-23 00:59:56 +0300

    Center body

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -5,12 +5,12 @@ function App() {
   return (
     // BEM naming convention
     <div className="app">
-      <h1>Hello!! This is WhatsApp clone 💦</h1>
 
       <div className="app__body">
         {/* Sidebar */}
         {/* Chat */}
       </div>
+      
     </div>
   );
 }
