8ba97dc4adff0142234669e355aa6db37094e38b
DeployChef <dc.titovvlad@gmail.com>
2020-09-23 00:49:08 +0300

    Prepair project

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,13 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBzBVCf9QXByYhsnkdvIqBg23oj0cVjUU0",
+    authDomain: "whatsappreactjsclone.firebaseapp.com",
+    databaseURL: "https://whatsappreactjsclone.firebaseio.com",
+    projectId: "whatsappreactjsclone",
+    storageBucket: "whatsappreactjsclone.appspot.com",
+    messagingSenderId: "780962329706",
+    appId: "1:780962329706:web:6d4d1cd5b40992acb1c613",
+    measurementId: "G-J3M7GJMDL3"
+  };
+
