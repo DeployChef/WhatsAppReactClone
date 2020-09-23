45b5d4d16dd93889aa3d65f3a4223d3aee40ce4c
DeployChef <dc.titovvlad@gmail.com>
2020-09-24 01:36:51 +0300

    init db, set rooms from db

--- whatsapp-clone/src/SidebarChat.js
+++ whatsapp-clone/src/SidebarChat.js
@@ -2,7 +2,7 @@ import { Avatar } from '@material-ui/core'
 import React, { useState, useEffect } from 'react'
 import './SidebarChat.css'
 
-function SidebarChat({ addNewChat }) {
+function SidebarChat({ addNewChat, id, name }) {
 
     const [seed, setSeed] = useState('');
 
@@ -22,7 +22,7 @@ function SidebarChat({ addNewChat }) {
         <div className="sidebarChat">
             <Avatar src={`https://avatars.dicebear.com/api/bottts/${seed}.svg`}/>
             <div className="sidebarChat__info">
-                <h2>Room name</h2>
+                <h2>{name}</h2>
                 <p>last message...</p>
             </div>
         </div>
--- whatsapp-clone/src/firebase.js
+++ whatsapp-clone/src/firebase.js
@@ -1,13 +1,19 @@
+import firebase from "firebase"
 
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBzBVCf9QXByYhsnkdvIqBg23oj0cVjUU0",
+  authDomain: "whatsappreactjsclone.firebaseapp.com",
+  databaseURL: "https://whatsappreactjsclone.firebaseio.com",
+  projectId: "whatsappreactjsclone",
+  storageBucket: "whatsappreactjsclone.appspot.com",
+  messagingSenderId: "780962329706",
+  appId: "1:780962329706:web:6d4d1cd5b40992acb1c613",
+  measurementId: "G-J3M7GJMDL3"
+})
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBzBVCf9QXByYhsnkdvIqBg23oj0cVjUU0",
-    authDomain: "whatsappreactjsclone.firebaseapp.com",
-    databaseURL: "https://whatsappreactjsclone.firebaseio.com",
-    projectId: "whatsappreactjsclone",
-    storageBucket: "whatsappreactjsclone.appspot.com",
-    messagingSenderId: "780962329706",
-    appId: "1:780962329706:web:6d4d1cd5b40992acb1c613",
-    measurementId: "G-J3M7GJMDL3"
-  };
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+const provider = new firebase.auth.GoogleAuthProvider();
 
+export { db, auth, storage, provider }
