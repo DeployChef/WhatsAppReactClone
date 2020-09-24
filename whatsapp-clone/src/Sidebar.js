f8e8f4e76b9b28eceedc1822420ef5e3e9b1dace
DeployChef <dc.titovvlad@gmail.com>
2020-09-24 23:12:57 +0300

    Add google photo

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './Sidebar.css'
 import DonutLargeIcom from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
@@ -7,10 +7,12 @@ import SearchOutlined from '@material-ui/icons/SearchOutlined';
 import { Avatar, IconButton } from '@material-ui/core';
 import SidebarChat from './SidebarChat'
 import { db } from './firebase';
+import { useStateValue } from './StateProvider';
 
 function Sidebar() {
 
     const [rooms, setRooms] = useState([]);
+    const [{ user }, dispatch] = useStateValue();
 
     useEffect(() => {
         const unsubscribe = db.collection('rooms').onSnapshot(snapshot => 
@@ -29,7 +31,7 @@ function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
-                <Avatar />
+                <Avatar src={user?.photoURL} />
                 <div className="sidebar__headerRight">
                     <IconButton>
                         <DonutLargeIcom />
