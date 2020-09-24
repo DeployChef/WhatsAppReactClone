78ffb9fc2062832df052a59fda1c8d2421086e6c
DeployChef <dc.titovvlad@gmail.com>
2020-09-24 23:23:17 +0300

    Get messages from db

--- whatsapp-clone/src/Chat.js
+++ whatsapp-clone/src/Chat.js
@@ -9,11 +9,18 @@ function Chat() {
     const [input, setInput] = useState('');
     const { roomId } = useParams();
     const [roomName, setRoomName] = useState('');
+    const [messages, setMessages] = useState([]);
 
     useEffect(() => {
         if (roomId) {
-            db.collection('rooms').doc(roomId).onSnapshot(snapshot=>(
+            db.collection('rooms').doc(roomId).onSnapshot(snapshot => (
                 setRoomName(snapshot.data().name)
+            ))
+
+            db.collection("rooms").doc(roomId)
+            .collection("messages").orderBy('timestamp', "asc")
+            .onSnapshot(snapshot => (
+                setMessages(snapshot.docs.map(doc => doc.data()))
             ))
         }
     }, [roomId])
@@ -48,19 +55,19 @@ function Chat() {
             </div>
 
             <div className="chat__body">
-                <p className={`chat__message ${true && "chat__reciever"}`}>
 
-                    <span className="chat__message__name">
-                        Name
-                    </span>
+                {messages.map(message => (
+                    <p className={`chat__message ${true && "chat__reciever"}`}>
+                        <span className="chat__message__name">
+                            {message.name}
+                        </span>
+                        {message.message}
+                        <span className="chat__message__timestamp">
+                            {new Date(message.timestamp?.toDate()).toUTCString()}
+                        </span>
+                    </p>
+                ))}
 
-                    Chat message
-
-                    <span className="chat__message__timestamp">
-                        3.12 pm
-                    </span>
-
-                </p>
             </div>
 
             <div className="chat__footer">
